4500cc7f5eb46939fc05e032757991a3e35bf7f7
tong-dai <td2471@princeton.edu>
2025-04-12 03:27:39 -0400

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Tong Dai on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitPracticeTests/GitPracticeTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitPracticeTests.swift
+//  GitPracticeTests
+//
+//  Created by Tong Dai on 4/12/25.
+//
+
+import Testing
+
+struct GitPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
